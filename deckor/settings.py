2c6911191e84a2d26e40200f380b2d33afba8560
kyrieerrvin <ervinraphaelalba@protonmail.com>
2023-07-27 22:10:08 +0800

    for deployment

--- deckor/settings.py
+++ deckor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e#jxx1c$w)@i!#5ztr2nz0&!n9w6!5eo)mw98@h+6w^ep9%ha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- website/views.py
+++ website/views.py
@@ -12,13 +12,17 @@ def contact(request):
 		message_email = request.POST['message-email']
 		message = request.POST['message']
 
+		contact_message = "<em><u><h3>This is an auto-generated response to your email sent to us.</h3></u></em> <br>Did you send this email: <br>" + message + "<br><br><br> <h4><strong>If you are aware of this message, reply with '<u>YES</u>'.</strong> <br> <strong>If you are not aware of this message, please <u>do not reply</h4></u>.</strong>"
+
+
 		# send an email
 		send_mail(
 			message_name, # subject
-			message + " \n\nSent by " + message_email, # message
+			contact_message, # message
 			message_email, # from email
-			['aethernether99@gmail.com'],
-			fail_silently=False # To Email
+			[message_email, 'aethernether99@gmail.com'],
+			fail_silently=False, # To Email
+			html_message=contact_message
 			)
 
 		return render(request, 'contact.html', {'message_name': message_name})
@@ -51,7 +55,7 @@ def appointment(request):
 		appointment = "Name: " + your_name + " \nPhone: " + your_phone + " \nEmail: " + your_email + " \nAddress: " + your_address + " \n\nSchedule: " + your_schedule + " \nAppointment date: " + formatted_date + " \nMessage: " + your_message
 
 		send_mail(
-			'Appointment Request', # subject
+			'Appointment Request Details', # subject
 			appointment, # message
 			your_email, # from email
 			[your_email, 'aethernether99@gmail.com'], # To Email
